25f321ee52138a5052af8b44e9d6c153312098eb
Deepak <budhirajadeepak123@gmail.com>
2024-08-12 22:55:56 +0530

    Basic Backend Setup

---
+++ backend/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const db = async ()=>{
+    try {
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("DB Connected");
+    } catch (error) {
+        console.log("Db Connection Error", error);
+    }
+}
+
+module.exports = {db};
